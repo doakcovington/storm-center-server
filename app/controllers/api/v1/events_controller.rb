e1e294952a148f556be9c78bfd2d301d0d4e1d25
doakcovington <doakcovington@protonmail.com>
2020-09-02 20:13:29 -0700

    Built out events routes

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,25 +1,32 @@
 class Api::V1::EventsController < ApplicationController
   
   def index
-
+    events = Event.all 
+    render json: events 
   end
 
   def show
-
+    event = Event.find(params[:id])
+    render json: event
   end
 
-
   def create
-
+    event = Event.new(event_params)
+    if event.save
+      render json: event
+    else 
+      render json: event.errors, status: unprocessable_entity
+    end
   end
 
   def destroy
-
+    event = Event.find(params[:id])
+    event.destroy
   end
 
   private
 
-  def events_params
+  def event_params
     params.require(:event).permit(:name, :description, :date, :location, :spaces, :tutor_id)
   end
 
