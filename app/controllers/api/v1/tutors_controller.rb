81afdbc427f4413a6db89170e3f7e427bff87b98
doakcovington <doakcovington@protonmail.com>
2020-09-02 20:19:12 -0700

    Built out tutor controllers

--- app/controllers/api/v1/tutors_controller.rb
+++ app/controllers/api/v1/tutors_controller.rb
@@ -1,18 +1,26 @@
 class Api::V1::TutorsController < ApplicationController
   def index
-
+    tutors = Tutor.all 
+    render json: tutors
   end
 
   def show
-
+    tutor = Tutor.find(params[:id])
+    render json: tutor
   end
 
   def create
-
+    tutor = Tutor.new(tutor_params)
+    if tutor.save
+      render json: tutor
+    else
+      render json: tutor.errors, status: unprocessable_entity
+    end
   end
 
   def destroy
-
+    tutor = Tutor.find(params[:id])
+    tutor.destroy
   end
 
   private
