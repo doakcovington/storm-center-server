4d475a1e2e89ebc7bed10736d0eaf18679a80ebb
doakcovington <doakcovington@protonmail.com>
2020-09-02 20:04:36 -0700

    Set up routes and strong params

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,2 +1,26 @@
-class Api::V1::EventsController < ApplicationController 
+class Api::V1::EventsController < ApplicationController
+  
+  def index
+
+  end
+
+  def show
+
+  end
+
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def events_params
+    params.require(:event).permit(:name, :description, :date, :location, :spaces, :tutor_id)
+  end
+
 end
--- app/controllers/api/v1/tutors_controller.rb
+++ app/controllers/api/v1/tutors_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::TutorsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def tutor_params
+    params.require(:tutor).permit(:name, :speciality, :phonenumber, :email)
+  end
 end
