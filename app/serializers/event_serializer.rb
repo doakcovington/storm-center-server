e2843912f824849492feef86640730b478fef0c4
doakcovington <doakcovington@protonmail.com>
2020-09-03 09:29:09 -0700

    Added tutor to event serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,4 +1,4 @@
 class EventSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :description, :date, :location, :spaces, :tutor_id
+  attributes :name, :description, :date, :location, :spaces, :tutor_id, :tutor 
 end
