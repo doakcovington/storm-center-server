04d191d5d182ec19d32b868f19f168666e588f15
doakcovington <doakcovington@protonmail.com>
2020-09-16 14:27:55 -0700

    Added icon_url to events

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,4 +1,4 @@
 class EventSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :description, :date, :location, :spaces, :time, :tutor_id, :tutor
+  attributes :name, :icon_url, :description, :date, :location, :spaces, :time, :tutor_id, :tutor
 end
