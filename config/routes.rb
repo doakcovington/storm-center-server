6d9a693af8d4dec6973639d2b09dcd07918e2831
doakcovington <doakcovington@protonmail.com>
2020-09-18 14:29:01 -0700

    Added admin create route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :events
       resources :tutors
+      resources :admins, only: [:create]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
