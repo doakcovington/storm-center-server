b2c4efadaec6dd705a39372c8e7a15e790e8dd5a
doakcovington <doakcovington@protonmail.com>
2020-09-18 14:04:35 -0700

    fixed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-heidi = User.create(name: "Heidi", email: "Heidi@email.com", password: "123")
+heidi = Admin.create(name: "Heidi", email: "Heidi@email.com", password: "123")
 
 doak = Tutor.create(name: 'Doak', speciality: 'Science', phonenumber: '555-555-5555', email:'doak@email.com', admin_id: 1)
 john = Tutor.create(name: 'John', speciality: 'Math', phonenumber: '444-444-4444', email:'john@email.com', admin_id: 1)
 
-biology = Event.create(name: 'Biology', description: 'Bio Study Group', date: DateTime.new(2020, 9, 8), location: 'zoom', spaces: 5, time: "3:30", tutor_id:1, admin_id: 1, icon_url: "https://www.flaticon.com/svg/static/icons/svg/880/880910.svg")
-zoom = Event.create(name: 'Zoom 101', description: 'Learn to zoom', date: DateTime.new(2020, 9, 8), location: 'zoom', spaces: 10, time: "2:30", tutor_id:2,  admin_id: 1, icon_url: "https://www.flaticon.com/svg/static/icons/svg/880/880910.svg")
+biology = Event.create(name: 'Biology', description: 'Bio Study Group', date: DateTime.new(2020, 9, 8), location: 'zoom', spaces: 5, time: "3:30", tutor_id:1, admin_id: heidi.id, icon_url: "https://www.flaticon.com/svg/static/icons/svg/880/880910.svg")
+zoom = Event.create(name: 'Zoom 101', description: 'Learn to zoom', date: DateTime.new(2020, 9, 8), location: 'zoom', spaces: 10, time: "2:30", tutor_id:2,  admin_id: heidi.id, icon_url: "https://www.flaticon.com/svg/static/icons/svg/880/880910.svg")
