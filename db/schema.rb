05c12e30fa999f27d95e81c1a977bf7aeac7222f
doakcovington <doakcovington@protonmail.com>
2020-09-21 20:41:04 -0700

    Removed index_tutors_on_admin_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_205122) do
+ActiveRecord::Schema.define(version: 2020_09_22_033558) do
 
   create_table "admins", force: :cascade do |t|
     t.string "name"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 2020_09_18_205122) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "admin_id"
-    t.index ["admin_id"], name: "index_tutors_on_admin_id"
   end
 
   add_foreign_key "events", "admins"
