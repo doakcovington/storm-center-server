abe48f7664f5785b77a7315648dd39a5739f14e7
doakcovington <doakcovington@protonmail.com>
2020-09-22 20:12:16 -0700

    built out decoded_token method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::API
   def encode_token(payload)
     JWT.encode(payload, 'pray_for_snow')
   end
+
+  def decode_token(token)
+    JWT.decode(token, 'pray_for_snow')[0]
+  end
+
 end
