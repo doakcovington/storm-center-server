999b1ce31a64d86ddc4ea55a220285921c7b6836
doakcovington <doakcovington@protonmail.com>
2020-09-23 20:19:24 -0700

    added token and skip_before_action

--- app/controllers/api/v1/admins_controller.rb
+++ app/controllers/api/v1/admins_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::AdminsController < ApplicationController
+
+  skip_before_action :authroized, only [:create] #skips authorized action to allow admin creation
+
   def create
     @admin = Admin.create(admin_params)
     if @admin.valid?
-      render json: { admin: AdminSerializer.new(@admin) }, status: :created
+      @token = encode_token(admin_id: @admin.id) #payload for encode_token method in application controller
+      render json: { admin: AdminSerializer.new(@admin), jwt: @token }, status: :created
     else
       render json: { error: 'failed to create admin' }, status: :not_acceptable
     end
