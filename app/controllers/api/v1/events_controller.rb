aedde4329ed439c67ef29e1c54c13a2c492399da
doakcovington <doakcovington@protonmail.com>
2020-09-29 13:56:04 -0700

    Added waitlist

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -32,7 +32,7 @@ class Api::V1::EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:name, :description, :date, :location, :spaces, :tutor_id, :time, :icon_url, :admin_id)
+    params.require(:event).permit(:name, :description, :date, :location, :spaces, :waitlist, :tutor_id, :time, :icon_url, :admin_id)
   end
 
 end
