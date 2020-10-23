a578931610128cf8df6e7d4dff3a60ea5507238f
doakcovington <doakcovington@protonmail.com>
2020-10-22 20:00:09 -0700

    fixed application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,40 +1,37 @@
 class ApplicationController < ActionController::API
+  before_action :authorized
 
-  #calls authorized before anything else
-  before_action :authorized 
-  
   def encode_token(payload)
-    JWT.encode(payload, 'pray_for_snow')
+    JWT.encode(payload, 'pray_4_snow')
   end
 
   def auth_header
     request.headers['Authorization']
   end
 
-  def decode_token(token)
+  def decoded_token
     if auth_header
       token = auth_header.split(' ')[1]
       begin
-        JWT.decode(token, 'pray_for_snow', true, algorithm: 'HS256')
+        JWT.decode(token, 'pray_4_snow', true, algorithm: 'HS256')
       rescue JWT::DecodeError
-        nill
+        nil
       end
     end
   end
 
-  def current_admin
-    if decode_token
-      admin_id = decode_token[0]['admin_id']
+  def logged_in_admin
+    if decoded_token
+      admin_id = decoded_token[0]['admin_id']
       @admin = Admin.find_by(id: admin_id)
     end
   end
 
   def logged_in?
-    !!current_admin #double bang (!!) returns boolean value
-  end     
-    
-  def authorized #Prompts user to log in if they are not authorized to view page or do action
-    render json: {message: 'Please Log In'}, status: :unauthorized unless logged_in?
+    !!logged_in_admin
   end
 
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
 end
