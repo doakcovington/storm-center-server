fa5b32f24ec01c16eca502b9605b3542fba3d88a
doakcovington <doakcovington@protonmail.com>
2020-10-27 13:18:56 -0700

    Built out login method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,15 @@ class UsersController < ApplicationController
         render json: {error: 'Invalid email or password'}
       end
   end
-  
+
+  def login
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: "Invalid email or password"}
+    end
+  end
+
 end
