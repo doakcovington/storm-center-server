a1f0f9d3e3a6af658a3345c9634e0c9dc2062b0d
doakcovington <doakcovington@protonmail.com>
2020-10-27 13:15:25 -0700

    Built out create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.create(user_params)
+      if @user.valid?
+        token = encode_token({user_id: @user.id})
+        render json: {user: @user, token: token}
+      else 
+        render json: {error: 'Invalid email or password'}
+      end
+  end
+  
 end
